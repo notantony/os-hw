2735c8231ac474c6efbe6849a8dcf547e01c4a91
notantony <not.antony@yandex.ru>
2018-06-08 22:47:24 +0300

    hw 7 added

--- ipc/client.cpp
+++ ipc/client.cpp
@@ -4,33 +4,80 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <cstring>
-
+#include <unistd.h>
 
 const size_t BUF_SIZE = 4096;
 char buffer[BUF_SIZE];
+const char *address = "test.sock";
+
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-		printf("Wrong format, usage: server <address>\n");
+    if (argc != 1) {
+		printf("Wrong format: no arguments expected\n");
 		return 0;
 	}
 	sockaddr server;
-	if (strlen(argv[1]) + 1 > sizeof(server.sa_data)) {
-		printf("Address is too long\n");
-		return 0;
-	}
-    
+
     int st;
-    if ((st = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
+    if ((st = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
 		fprintf(stderr, "Cannot create socket, error code: %d\n", errno);
 		return EXIT_FAILURE;
 	}
+
 	server.sa_family = AF_UNIX;
-	strcpy(server.sa_data, argv[1]);
-    while(scanf("%s", buffer) != EOF) {
-        if (sendto(st, buffer, strlen(buffer), 0, &server, sizeof(server.sa_family) + strlen(server.sa_data)) == -1) {
-            fprintf(stderr, "Error while sending message, error code: %d\n", errno);
-        }
-    }
+	strcpy(server.sa_data, address);
+	socklen_t server_len  = sizeof(server.sa_family) + strlen(server.sa_data);
+
+	if (connect(st, &server, server_len) == -1) {
+		fprintf(stderr, "Cannot connect to server, error code: %d\n", errno);
+		return EXIT_FAILURE;
+	}
+
+	printf("Connection established\n");
+	
+    char buf[1];
+    struct iovec iov;
+    struct msghdr msg;
+    struct cmsghdr *cmsg;
+    char cms[CMSG_SPACE(sizeof(int))];
+
+    iov.iov_base = buf;
+    iov.iov_len = sizeof(buf);
+
+    msg.msg_name = 0;
+    msg.msg_namelen = 0;
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    msg.msg_flags = 0;
+    msg.msg_control = (caddr_t) cms;
+    msg.msg_controllen = sizeof cms;
+
+
+	ssize_t msg_len;
+    if (msg_len = recvmsg(st, &msg, 0) == -1) {
+		fprintf(stderr, "Error while receiving pipe file descriptor\n");
+		return -1;
+	}
+
+    int pipe_fd;
+    cmsg = CMSG_FIRSTHDR(&msg); 
+    memmove(&pipe_fd, CMSG_DATA(cmsg), sizeof(int));
+
+	const char *hello = "Hello, this is pipe";
+	if (write(pipe_fd, hello, strlen(hello)) == -1) {
+		fprintf(stderr, "Error while sending message, error code: %d\n", errno);
+	}
+    while (scanf("%s", buffer) != EOF) {
+		if (write(pipe_fd, buffer, strlen(buffer)) == -1) {
+			fprintf(stderr, "Error while sending message, error code: %d\n", errno);
+		}
+		if (!strcmp(buffer, "Bye")) {
+			return 0;
+		}
+	}
+	const char *bye = "Bye";
+	if (write(pipe_fd, bye, strlen(bye)) == -1) {
+		fprintf(stderr, "Error while sending message, error code: %d\n", errno);
+	}
     return 0;
 }
--- ipc/server.cpp
+++ ipc/server.cpp
@@ -7,60 +7,110 @@
 #include <unistd.h>
 
 
+
 const size_t BUF_SIZE = 4096;
 char buffer[BUF_SIZE];
-char *socket_name = "game_socket";
+const char *address = "test.sock";
+
 
 int main(int argc, char **argv) {
 	if (argc != 1) {
-		printf("No arguments expected\n");
+		printf("Wrong format: no arguments expected\n");
 		return 0;
 	}
 	sockaddr server;
 
 	int st;
-	if ((st = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1) {
+	if ((st = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
 		fprintf(stderr, "Cannot create socket, error code: %d\n", errno);
 		return EXIT_FAILURE;
 	}
 
 	server.sa_family = AF_UNIX;
-	strcpy(server.sa_data, socket_name);
+	strcpy(server.sa_data, address);
+	socklen_t server_len  = sizeof(server.sa_family) + strlen(server.sa_data);
 	
-	if (bind(st, &server, sizeof(server.sa_family) + strlen(server.sa_data)) == -1) {
+	if (bind(st, &server, server_len) == -1) {
 		fprintf(stderr, "Error while binding, error code: %d\n", errno);
 		return EXIT_FAILURE;
 	}
 	printf("Server started\n");
 
-	int i_pipe[2];
-	int o_pipe[2];
-	if (pipe(i_pipe) == -1 || pipe(o_pipe) == -1) {
-		fprintf(stderr, "Cannot create pipe");
+	int in_pipe[2];
+	if (pipe(in_pipe) == -1) {
+		fprintf(stderr, "Cannot create pipe, error code: %d", errno);
 		return EXIT_FAILURE;
 	}
 
-	ssize_t len;
+	listen(st, 10);
 	while (1) {
-		if ((len = recvfrom(st, buffer, BUF_SIZE, 0, NULL, NULL)) < 0) {
-			fprintf(stderr, "Error while receiving message, error code: %d\n", errno);
+		int connected;
+		if ((connected = accept(st, &server, &server_len)) == -1) {
+			fprintf(stderr, "Cannot establish connection, error code: %d\n", errno);
 			continue;
 		}
-		buffer[len] = 0;
-		if (!strcmp(buffer, "exit")) {
-			printf("Shutting down server\n");
-			break;
-		}
-		printf("%s connected\n", buffer);
+		printf("Someone connected\n");
+		fflush(stdout);
+
+    	struct msghdr msg;
+		memset(&msg, 0, sizeof(msghdr));
+		struct cmsghdr *cmsg;
+		int *fdptr;
 		
+		char iobuf[1];
+		struct iovec io;
+		io.iov_base = iobuf;
+		io.iov_len = sizeof(iobuf);
+		
+		union {
+			char buf[CMSG_SPACE(sizeof(int *))];
+			struct cmsghdr align;
+		} u;
+
+		msg.msg_iov = &io;
+		msg.msg_iovlen = 1;
+		msg.msg_control = u.buf;
+		msg.msg_controllen = sizeof(u.buf);
+
+		cmsg = CMSG_FIRSTHDR(&msg);
+		cmsg->cmsg_level = SOL_SOCKET;
+		cmsg->cmsg_type = SCM_RIGHTS;
+		cmsg->cmsg_len = CMSG_LEN(sizeof(int));
+		fdptr = (int *) CMSG_DATA(cmsg);
+		memcpy(fdptr, &in_pipe[1], sizeof(int));
+
+    	if (sendmsg(connected, &msg, 0) == -1) {
+			fprintf(stderr, "Error while sending file descriptor\n");		
+			printf("Disconnected\n");
+			fflush(stdout);
+			continue;
+		}
+		
+		do {
+			ssize_t msg_len = read(in_pipe[0], buffer, BUF_SIZE);
+			buffer[msg_len] = 0;
+			fprintf(stderr, "%s\n", buffer);
+		} while (strcmp(buffer, "Bye"));
+
+		printf("Disconnected\n");
+		fflush(stdout);
+
+		if (close(connected) == -1) {
+			fprintf(stderr, "Cannot close file descriptor for input connection, error code: %d\n", errno);
+		}
+	}
+	int exit_code = 0;
+	if (close(in_pipe[0] == -1)) {
+		fprintf(stderr, "Cannot close socket, error code, error code: %d\n", errno);
+		exit_code = EXIT_FAILURE;
 	}
 	if (close(st) == -1) {
 		fprintf(stderr, "Cannot close socket, error code, error code: %d\n", errno);
-		return EXIT_FAILURE;
+		exit_code = EXIT_FAILURE;
 	}
-	if (unlink(socket_name) == -1) {
+	if (unlink(address) == -1) {
 		fprintf(stderr, "Cannot delete socket file, error code: %d\n", errno);
-		return EXIT_FAILURE;
+		exit_code = EXIT_FAILURE;
 	}
 	return 0;
 }
